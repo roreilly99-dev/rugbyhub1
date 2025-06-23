21cb9009287462d6d71724de1df7857ab080d2fb
roreilly99 <rory.oreilly@ucd.ie>
2025-06-23 21:01:30 +0100

    Commiting first Docker file

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,12 +1,71 @@
+'use client';
+
+import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import ProductCard from "./components/ProductCard";
 
 export default function Home() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
-    <main>
-      <h1>Hello World</h1>
-      <Link href = "/users">Users</Link>
-      <ProductCard />
-    </main>
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+        </div>
+        <div className="flex-1">
+          <Link href="/">
+            <Image
+              src="/RugbyHub Logo Design.png"
+              alt="RugbyHub Logo"
+              width={150}
+              height={20}
+              className="cursor-pointer"
+            />
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <button className="btn btn-ghost btn-circle" onClick = {() =>setShowSearch((prev) => !prev)}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
+          </button>
+          {showSearch && (<input type="text" placeholder="Search..." className="input" />)}
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+        </div>
+      </div>
+      <div className="flex w-full flex-col">
+        <div className="card bg-base-300 rounded-box grid h-20 place-items-center">content</div>
+        <div className="divider"></div>
+        <div className="card bg-base-300 rounded-box grid h-20 place-items-center">content</div>
+      </div>
+      <div className="dock">
+        <button>
+          <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="currentColor" stroke-linejoin="miter" stroke-linecap="butt"><polyline points="1 11 12 2 23 11" fill="none" stroke="currentColor" stroke-miterlimit="10" strokeWidth="2"></polyline><path d="m5,13v7c0,1.105.895,2,2,2h10c1.105,0,2-.895,2-2v-7" fill="none" stroke="currentColor" stroke-linecap="square" stroke-miterlimit="10" strokeWidth="2"></path><line x1="12" y1="22" x2="12" y2="18" fill="none" stroke="currentColor" stroke-linecap="square" stroke-miterlimit="10" strokeWidth="2"></line></g></svg>
+          <span className="dock-label">Home</span>
+        </button>
+        <button className="dock-active">
+          <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="currentColor" stroke-linejoin="miter" stroke-linecap="butt"><polyline points="3 14 9 14 9 17 15 17 15 14 21 14" fill="none" stroke="currentColor" stroke-miterlimit="10" strokeWidth="2"></polyline><rect x="3" y="3" width="18" height="18" rx="2" ry="2" fill="none" stroke="currentColor" stroke-linecap="square" stroke-miterlimit="10" strokeWidth="2"></rect></g></svg>
+          <span className="dock-label">Inbox</span>
+        </button>
+        
+        <button>
+          <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="currentColor" stroke-linejoin="miter" stroke-linecap="butt"><circle cx="12" cy="12" r="3" fill="none" stroke="currentColor" stroke-linecap="square" stroke-miterlimit="10" strokeWidth="2"></circle><path d="m22,13.25v-2.5l-2.318-.966c-.167-.581-.395-1.135-.682-1.654l.954-2.318-1.768-1.768-2.318.954c-.518-.287-1.073-.515-1.654-.682l-.966-2.318h-2.5l-.966,2.318c-.581.167-1.135.395-1.654.682l-2.318-.954-1.768,1.768.954,2.318c-.287.518-.515,1.073-.682,1.654l-2.318.966v2.5l2.318.966c.167.581.395,1.135.682,1.654l-.954,2.318,1.768,1.768,2.318-.954c.518.287,1.073.515,1.654.682l.966,2.318h2.5l.966-2.318c.581-.167,1.135-.395,1.654-.682l2.318.954,1.768-1.768-.954-2.318c.287-.518.515-1.073.682-1.654l2.318-.966Z" fill="none" stroke="currentColor" stroke-linecap="square" stroke-miterlimit="10" strokeWidth="2"></path></g></svg>
+          <span className="dock-label">Settings</span>
+        </button>
+      </div>
+      <main>
+        <Link href="/users">Users</Link>
+        <ProductCard />
+      </main>
+    </>
   );
 }
